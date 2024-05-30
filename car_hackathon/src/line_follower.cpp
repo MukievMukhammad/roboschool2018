e0cf58c1528c7ee485d18f7b21ef66361aa81656
MukievMukhammad <madani200160@gmail.com>
2024-05-30 13:32:33 +0300

    PID

--- car_hackathon/src/line_follower.cpp
+++ car_hackathon/src/line_follower.cpp
@@ -31,11 +31,56 @@ void image_callback(const sensor_msgs::ImageConstPtr& msg)
     cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     cv::Mat image = cv_ptr->image;
 
-    // TODO: control the motors
-    // motors(50,50);
+    cv::Mat hsvImage;
+    cv:cvtColor(image, hsvImage, CV_BGR2HSV);
 
-    cv::imshow("img", image);
-    cv::waitKey(1);
+    // Threshold the HSV image, keep only the red pixels
+    cv::Mat mask;
+    cv::Scalar lower_red(115, 110, 20);
+    cv::Scalar upper_red(195, 255, 140);
+    cv::inRange(hsvImage, lower_red, upper_red, mask);
+
+    int width = mask.cols;
+    int height = mask.rows;
+
+    int search_top = height / 2;
+    int search_bottom = search_top + 150;
+
+    // Zero out pixels outside the desired region
+    for (int y = 0; y < height - 2; y++) {
+        if (y < search_top || y > search_bottom) {
+            for (int x = 0; x < width; x++) {
+                mask.at<cv::Vec3b>(y, x)[0] = 0;
+                mask.at<cv::Vec3b>(y, x)[1] = 0;
+                mask.at<cv::Vec3b>(y, x)[2] = 0;
+            }
+        }
+    }
+
+    cv::Moments M = cv::moments(mask);
+
+    if (M.m00 > 0) {
+        int cx = int(M.m10 / M.m00);
+        int cy = int(M.m01 / M.m00);
+        cv::circle(image, cv::Point(cx, cy), 20, CV_RGB(255, 0, 0), -1);
+
+        // Move the robot in proportion to the error signal
+        int err = cx - width / 2;
+
+        if (err<cx)
+            motors(10+err/65,10);
+    }
+    else 
+    {
+        motors(2,0);
+    }
+    
+    // Update the GUI window
+    cv::resize(mask, mask, cv::Size(width, height));
+    cv::imshow("img", mask);
+    cv::waitKey(3);
+    // cv::imshow("img", image);
+    // cv::waitKey(1);
 }
 
 void motors(int16_t left, int16_t right)
